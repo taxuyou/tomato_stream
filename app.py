105b3323ba048d5c1efa836d0e5d198c92f7e345
taxuyou <53686222+taxuyou@users.noreply.github.com>
2022-12-28 16:16:56 +0900

    3

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ st.title('토마토 병해 예측기')
 
 def main() :
     add_bg_from_url()
-    file_uploaded = st.file_uploader('Choose an image...', type = ['jpg','jpeg','png'])
+    file_uploaded = st.file_uploader('사진을 선택해주세요...', type = ['jpg','jpeg','png'])
     if file_uploaded is not None :
         image = Image.open(file_uploaded)
         st.write("Uploaded Image.")
-        figure = plt.figure(figsize = (5,5))
+        figure = plt.figure(figsize = (4,4))
         plt.imshow(image)
         plt.axis('off')
         st.pyplot(figure)
@@ -34,7 +34,7 @@ def main() :
         #st.success(st.write('Confidence : {}%'.format(confidence)))
 
 def predict_class(image) :
-    with st.spinner('Loading Model...'):
+    with st.spinner('모델을 불러오고 있습니다...'):
         classifier_model = keras.models.load_model(r'tomatos.h5', compile = False)
 
     shape = ((256,256,3))
@@ -43,14 +43,8 @@ def predict_class(image) :
     test_image = keras.preprocessing.image.img_to_array(test_image)
     test_image /= 255.0
     test_image = np.expand_dims(test_image, axis = 0)
-    class_name = ['Bacterial_spot(반점세균병)','Early_blight(겹무늬병)',
-               'Late_blight(잎마름역병)','Leaf_Mold(잎곰팡이병)',
-               'Septoria_leaf_spot(흰무늬병)',
-               'Spider_mites_Two_spotted_spider_mite(점박이응애)',
-               'Target_Spot(갈색무늬병)',
-               'YellowLeaf_Curl_Virus(황화잎말림바이러스)',
-               'mosaic_virus(모자이크병)',
-               'healthy(정상)']
+    class_name = ['Bacterial_spot(반점세균병)','Early_blight(겹무늬병)','Late_blight(잎마름역병)','Leaf_Mold(잎곰팡이병)','Septoria_leaf_spot(흰무늬병)',
+    'Spider_mites_Two_spotted_spider_mite(점박이응애)','Target_Spot(갈색무늬병)','YellowLeaf_Curl_Virus(황화잎말림바이러스)','mosaic_virus(모자이크병)','healthy(정상)']
 
     prediction = model.predict(test_image)
     confidence = round(100 * (np.max(prediction[0])), 2)
@@ -63,7 +57,7 @@ def add_bg_from_url():
          <style>
          .stApp {{
           
-             background-image: url("https://www.cstimes.com/news/photo/202206/503679_406026_4546.png");
+             background-image: url("http://cdn.itdaily.kr/news/photo/202103/202318_202307_3426.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
